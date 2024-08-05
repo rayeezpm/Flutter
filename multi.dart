78fc1f79aa31620d71acee37dd997f2763cf21d4
rayeezpm <rayeez.pm.new@gmail.com>
2024-08-05 17:14:07 +0530

    Third commit

---
+++ multi.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+
+void main() {
+  print('Enter first number:');
+  int? n1 = int.parse(stdin.readLineSync()!);
+  print('Enter second number');
+  int? n2 = int.parse(stdin.readLineSync()!);
+  int mul = n1 * n2;
+  print('Multiple  = $mul');
+}
