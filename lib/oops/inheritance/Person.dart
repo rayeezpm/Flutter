25d476d9e8e546ac4b7e56a7f39ec5f637daf359
rayeezpm <rayeez.pm.new@gmail.com>
2024-08-23 12:36:07 +0530

    oops commit

---
+++ lib/oops/inheritance/Person.dart
@@ -0,0 +1,22 @@
+class Person {
+  String? name;
+  int? age;
+}
+
+class Student extends Person {
+  Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  void display() {
+    print('Name: $name \nage: $age');
+  }
+}
+
+void main() {
+  Student student = Student();
+  student.name = 'Rick';
+  student.age = 14;
+  student.display();
+}
